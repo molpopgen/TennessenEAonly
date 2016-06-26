3fa2a678428903608e7fb681b7e237905a331096
molpopgen <krthornt@uci.edu>
2016-06-26 15:06:13 -0700

    update to use random seed

--- subStats.sh
+++ subStats.sh
@@ -5,17 +5,17 @@ sampler=$1
 
 model=gbr
 
-seed=101
+seed=$RANDOM
 
 for lambda in 0.025 0.05 0.1 0.25 0.5
 do
     ofile=$model."lambda"$lambda.$sampler.h5
     name=$model$sampler
     qsub -N $name runSims.sh $model $lambda $ofile $seed 1.0 $sampler
-    seed=$(($seed*2))
+    seed=$RANDOM
 done
 
-seed=101
+seed=$RANDOM
 for model in additive multi
 do
     for lambda in 0.025 0.05 0.1 0.25 0.5
@@ -25,7 +25,7 @@ do
 	    ofile=$model."lambda"$lambda.$sampler.h5
 	    name=$model$sampler
 	    qsub -N $name runSims.sh $model $lambda $ofile $seed 1.0 $sampler
-	    seed=$(($seed*2))
+	    seed=$RANDOM
 	done
     done
 done
