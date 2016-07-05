88c42a9895269f1c356e2af87afa17d8fefbcfb9
molpopgen <krthornt@uci.edu>
2016-07-05 15:40:58 -0700

    mod script to submit all models w/params we want

--- subVA.sh
+++ subVA.sh
@@ -3,20 +3,23 @@
 sampler=VA
 #Remember: GBR model = dominance doesn't matter
 
-model=gbr
+
 
 seed=$RANDOM
 
-for lambda in 0.025 0.05 0.1 0.25 0.5
+for model in additive #gbr
 do
-    ofile=$model."lambda"$lambda.$sampler.h5
-    name=$model$sampler
-    qsub -N $name runSimsVA.sh $model $lambda $ofile $seed 1.0 $sampler
-    seed=$RANDOM
+    for lambda in 0.025 0.05 0.1 0.25 0.5
+    do
+	ofile=$model."lambda"$lambda.$sampler.h5
+	name=$model$sampler
+	qsub -N $name runSimsVA.sh $model $lambda $ofile $seed 1.0 $sampler
+	seed=$RANDOM
+    done
 done
 
 seed=$RANDOM
-for model in additive multi
+for model in multi
 do
     for lambda in 0.025 0.05 0.1 0.25 0.5
     do
@@ -24,7 +27,7 @@ do
 	do
 	    ofile=$model."lambda"$lambda.$sampler.h$dominance.h5
 	    name=$model$sampler
-	    #qsub -N $name runSimsVA.sh $model $lambda $ofile $seed 1.0 $sampler
+	    qsub -N $name runSimsVA.sh $model $lambda $ofile $seed 1.0 $sampler
 	    seed=$RANDOM
 	done
     done
