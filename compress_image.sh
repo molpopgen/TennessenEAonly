85ffe2f9d928535dc7fc6cd1b2781619a7160b98
molpopgen <krthornt@uci.edu>
2016-07-19 11:46:54 -0700

    tweak stats figs

--- compress_image.sh
+++ compress_image.sh
@@ -4,3 +4,8 @@ convert TennessenVA.tif -set colorspace RGB -layers flatten -alpha off -compress
 -density 600 -adaptive-resize 4500x2400  TennessenVA_compressed.tif
 
 convert TennessenVA_compressed.tif TennessenVA_compressed.pdf
+
+convert TennessenStats.tif -set colorspace RGB -layers flatten -alpha off -compress lzw -depth 8 \
+-density 600 -adaptive-resize 4500x2400  TennessenStats_compressed.tif
+
+convert TennessenStats_compressed.tif TennessenStats_compressed.pdf
