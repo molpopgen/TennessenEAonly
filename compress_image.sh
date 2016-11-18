11b71082e903d6a325017c13a5ef07999d116af0
jsanjak <jsanjak@uci.edu>
2016-11-18 15:49:07 -0800

    split out segs/fixes and added burden ratio

--- compress_image.sh
+++ compress_image.sh
@@ -9,3 +9,24 @@ convert TennessenStats.tif -set colorspace RGB -layers flatten -alpha off -compr
 -density 600 -adaptive-resize 4500x2400  TennessenStats_compressed.tif
 
 convert TennessenStats_compressed.tif TennessenStats_compressed.pdf
+
+
+convert AllLoads.tif -set colorspace RGB -layers flatten -alpha off -compress lzw -depth 8 \
+-density 600 -adaptive-resize 4500x2400  AllLoads_compressed.tif
+
+convert AllLoads_compressed.tif AllLoads_compressed.pdf
+
+convert BurdenRatioLoad.tif -set colorspace RGB -layers flatten -alpha off -compress lzw -depth 8 \
+-density 600 -adaptive-resize 4500x2400  BurdenRatioLoad_compressed.tif
+
+convert BurdenRatioLoad_compressed.tif BurdenRatioLoad_compressed.pdf
+
+convert AllMutations.tif -set colorspace RGB -layers flatten -alpha off -compress lzw -depth 8 \
+-density 600 -adaptive-resize 4500x2400  AllMutations_compressed.tif
+
+convert AllMutations_compressed.tif AllMutations_compressed.pdf
+
+convert BurdenRatioMutations.tif -set colorspace RGB -layers flatten -alpha off -compress lzw -depth 8 \
+-density 600 -adaptive-resize 4500x2400  BurdenRatioMutations_compressed.tif
+
+convert BurdenRatioMutations_compressed.tif BurdenRatioMutations_compressed.pdf
